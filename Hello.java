2ee13c660148bca780ee4c1683608922282f3a19
MattFrayser <98035703+MattFrayser@users.noreply.github.com>
2022-01-19 09:40:46 -0500

    Add files via upload
    Lab 1

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/********************************************************
+* Hello
+*********************************************************
+* Matt Frayser
+* 1/19/22
+* CMSC 255
+*********************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Matt Frayser");
+		System.out.println("Hello");
+		System.out.println("Lab 1");
+		System.out.println("1/19/22");
+		System.out.println("CMSC-255-003");
+		System.out.println();
+	}
+}
